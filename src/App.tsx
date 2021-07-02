bb093a6e9f7c08858401a0550d6faeefea638524
Devendra Pratap Singh <59687571+devendradbdigital@users.noreply.github.com>
2021-07-02 21:36:30 +0530

    dev name

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <>
       <div className="wrapper">
         <h1 className={Styles.heading}>
-          Hello! Dev <span> {process.env.NODE_ENV} </span>
+          Hello! Dev Singh New <span> {process.env.NODE_ENV} </span>
           {process.env.name}
           <br />
           <img src={codeIcon} alt="code" width="200" />
